6bd93babb0fea68d8c07dc51c6680a479f00d4e6
boncat <bonarbonbon@gmail.com>
2014-12-01 16:19:19 +0700

    add new feature maintanance

--- hino.mobile.park/src/com/plaxto/hino/mobile/park/AndroidDashboardDesignActivity.java
+++ hino.mobile.park/src/com/plaxto/hino/mobile/park/AndroidDashboardDesignActivity.java
@@ -1,143 +1,168 @@
 package com.plaxto.hino.mobile.park;
 
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+public class AndroidDashboardDesignActivity extends Activity
+{
 
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard_layout);
 
+		/**
+		 * Creating all buttons instances
+		 * */
+		// Dashboard News feed button
+		Button btn_newsfeed = (Button) findViewById(R.id.btn_news_feed);
 
-public class AndroidDashboardDesignActivity extends Activity {
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard_layout);
-        
-        /**
-         * Creating all buttons instances
-         * */
-        // Dashboard News feed button
-        Button btn_newsfeed = (Button) findViewById(R.id.btn_news_feed);
-        
-        // Dashboard Friends button
-        Button btn_friends = (Button) findViewById(R.id.btn_friends);
-        
-        // Dashboard Messages button
-        Button btn_messages = (Button) findViewById(R.id.btn_messages);
-        
-        // Dashboard Places button
-        Button btn_places = (Button) findViewById(R.id.btn_places);
-        
-        Button btn_events = (Button) findViewById(R.id.btn_events);
-        
-        Button btn_photos = (Button) findViewById(R.id.btn_photos);
-        
-      
-        Button btn_transfer_out = (Button) findViewById(R.id.btn_transfer_out);
-        Button btn_transfer_in = (Button) findViewById(R.id.btn_transfer_in);
-        // Dashboard Events button
-      
-        /**
-         * Handling all button click events
-         * */
-        
-        // Listening to News Feed button click
-        btn_newsfeed.setOnClickListener(new View.OnClickListener() {
-			
+		// Dashboard Friends button
+		Button btn_friends = (Button) findViewById(R.id.btn_friends);
+
+		// Dashboard Messages button
+		Button btn_messages = (Button) findViewById(R.id.btn_messages);
+
+		// Dashboard Places button
+		Button btn_places = (Button) findViewById(R.id.btn_places);
+
+		Button btn_events = (Button) findViewById(R.id.btn_events);
+
+		Button btn_photos = (Button) findViewById(R.id.btn_photos);
+
+		Button btn_transfer_out = (Button) findViewById(R.id.btn_transfer_out);
+		
+		Button btn_transfer_in = (Button) findViewById(R.id.btn_transfer_in);
+		
+		Button btn_maintanance = (Button) findViewById(R.id.button_maintanance);
+		// Dashboard Events button
+
+		/**
+		 * Handling all button click events
+		 * */
+
+		// Listening to News Feed button click
+		btn_newsfeed.setOnClickListener(new View.OnClickListener()
+		{
+
 			@Override
-			public void onClick(View view) {
+			public void onClick(View view)
+			{
 				// Launching News Feed Screen
 				Intent i = new Intent(AndroidDashboardDesignActivity.this, listWaitingPdi.class);
 				startActivity(i);
 				finish();
 			}
 		});
-        
-       // Listening Friends button click
-        btn_friends.setOnClickListener(new View.OnClickListener() {
-			
+
+		// Listening Friends button click
+		btn_friends.setOnClickListener(new View.OnClickListener()
+		{
+
 			@Override
-			public void onClick(View view) {
+			public void onClick(View view)
+			{
 				// Launching News Feed Screen
 				Intent i = new Intent(AndroidDashboardDesignActivity.this, OutOffice.class);
 				startActivity(i);
 				finish();
 			}
 		});
-        
-        // Listening Messages button click
-        btn_messages.setOnClickListener(new View.OnClickListener() {
-			
+
+		// Listening Messages button click
+		btn_messages.setOnClickListener(new View.OnClickListener()
+		{
+
 			@Override
-			public void onClick(View view) {
+			public void onClick(View view)
+			{
 				Intent i = new Intent(AndroidDashboardDesignActivity.this, MainActivity.class);
 				startActivity(i);
 				finish();
 			}
 		});
-        
-        // Listening to Places button click
-        btn_places.setOnClickListener(new View.OnClickListener() {
-			
+
+		// Listening to Places button click
+		btn_places.setOnClickListener(new View.OnClickListener()
+		{
+
 			@Override
-			public void onClick(View view) {
+			public void onClick(View view)
+			{
 				// Launching News Feed Screen
 				Intent i = new Intent(AndroidDashboardDesignActivity.this, introCheckOutPdi.class);
 				startActivity(i);
 				finish();
 			}
 		});
-        
-        btn_events.setOnClickListener(new View.OnClickListener() {
-			
+
+		btn_events.setOnClickListener(new View.OnClickListener()
+		{
+
 			@Override
-			public void onClick(View view) {
+			public void onClick(View view)
+			{
 				// Launching News Feed Screen
 				Intent i = new Intent(getApplicationContext(), listStockEvent.class);
 				startActivity(i);
 			}
 		});
-        
-        btn_photos.setOnClickListener(new View.OnClickListener() {
-			
+
+		btn_photos.setOnClickListener(new View.OnClickListener()
+		{
+
 			@Override
-			public void onClick(View view) {
+			public void onClick(View view)
+			{
 				// Launching News Feed Screen
 				Intent i = new Intent(getApplicationContext(), MainActivityParkingCheck.class);
 				startActivity(i);
 				finish();
 			}
 		});
-        
-        
-        
-        btn_transfer_out.setOnClickListener(new View.OnClickListener() {
-			
+
+		btn_transfer_out.setOnClickListener(new View.OnClickListener()
+		{
+
 			@Override
-			public void onClick(View view) {
+			public void onClick(View view)
+			{
 				// Launching News Feed Screen
 				Intent i = new Intent(getApplicationContext(), transferOut.class);
 				startActivity(i);
 				finish();
 			}
 		});
-        btn_transfer_in.setOnClickListener(new View.OnClickListener() {
-			
+		btn_transfer_in.setOnClickListener(new View.OnClickListener()
+		{
+
 			@Override
-			public void onClick(View view) {
+			public void onClick(View view)
+			{
 				// Launching News Feed Screen
 				Intent i = new Intent(getApplicationContext(), transferIn.class);
 				startActivity(i);
 				finish();
 			}
 		});
-        
-        // Listening to Events button click
-        
-    }
+
+		// Listening to Events button click
+		btn_maintanance.setOnClickListener(new View.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), ListWaitingMaintanance.class);
+				startActivity(intent);
+			}
+		});
+		
+
+	}
 }
---
+++ hino.mobile.park/src/com/plaxto/hino/mobile/park/ListWaitingMaintanance.java
@@ -0,0 +1,14 @@
+package com.plaxto.hino.mobile.park;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class ListWaitingMaintanance extends ListActivity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list_waiting_maintanance);
+	}
+}
